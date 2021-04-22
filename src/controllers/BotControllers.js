7e76fe0de0431754382826065db33afd7c6e1997
ando-ramanamihanta <ramanamihanta.ando@gmail.com>
2021-04-22 16:32:15 +0300

    update webhook

--- src/controllers/BotControllers.js
+++ src/controllers/BotControllers.js
@@ -1,3 +1,5 @@
+const request = require("request");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -39,6 +41,14 @@ export const postWebhook = async (req, res) => {
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Return a '200 OK' response to all events
@@ -50,10 +60,78 @@ export const postWebhook = async (req, res) => {
 };
 
 // Handles messages events
-export const handleMessage = (sender_psid, received_message) => {};
+export const handleMessage = (sender_psid, received_message) => {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+    };
+  } else if (received_message.attachments) {
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+};
 
 // Handles messaging_postbacks events
 export const handlePostback = (sender_psid, received_postback) => {};
 
 // Sends response messages via the Send API
-export const callSendAPI = (sender_psid, response) => {};
+export const callSendAPI = (sender_psid, response) => {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v6.0/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+};

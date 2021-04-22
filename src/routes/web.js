6ad5c6ba05a9298e4bf117333adc4c09c6879a9f
ando-ramanamihanta <ramanamihanta.ando@gmail.com>
2021-04-22 15:13:14 +0300

    update webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { getHome } = require("../controllers/HomeControllers");
+const { getWebhook, postWebhook } = require("../controllers/BotControllers");
 
 export const initWebRoutes = (app) => {
   router.get("/", getHome);
+  router.get("/webhook", getWebhook);
+  router.post("/webhook", postWebhook);
 
   return app.use("/", router);
 };
